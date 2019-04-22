9e2703c0c6b0bde5eee03e1b2d3194e0df82ce05
kajweb <kajweb@163.com>
2019-04-23 02:35:39 +0800

    fx

---
+++ runPHPServer.bat
@@ -0,0 +1,10 @@
+@echo off
+cd  %~dp0
+echo.
+echo                          ********
+echo                          *      *
+echo try run http://localhost:* 9630 *;
+echo                          *      *
+echo                          ********
+echo.
+php -S 0.0.0.0:9630
--- runServer.bat
+++
@@ -1,4 +0,0 @@
-cd  %~dp0
-
-echo "try run http://localhost:9630";
-php -S 0.0.0.0:9630
